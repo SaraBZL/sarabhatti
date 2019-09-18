962a03f86382f0e292e68aded58ba7a84066c80c
SaraBZL <36004934+SaraBZL@users.noreply.github.com>
2019-09-18 13:59:25 -0400

    update

--- header.php
+++ header.php
@@ -1,7 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<meta charset="UTF-8">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+   		"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"> 
+<title><?php echo $title; ?></title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
